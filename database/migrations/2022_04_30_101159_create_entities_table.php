de3619725ec2739fd197db99e2ee58fed1cbb0cc
ToniGomez <agomezj@cifpfbmoll.eu>
2022-04-30 13:07:53 +0200

    Add entities and roles tables with its relational tables

---
+++ database/migrations/2022_04_30_101159_create_entities_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->date('formdate');
+            $table->boolean('status');
+            $table->string("image");
+            $table->unsignedBigInteger('country_id');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+
+            $table->foreign('country_id')
+                ->references('id')
+                ->on('countries')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entities');
+    }
+}
---
+++ database/migrations/2022_04_30_102331_create_entities_role_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntitiesRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entities_role', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('entities_id');
+            $table->unsignedBigInteger('roles_id');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+
+            $table->foreign('entities_id')
+                ->references('id')
+                ->on('entities')
+                ->onDelete('cascade');
+            $table->foreign('roles_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entities_role');
+    }
+}
