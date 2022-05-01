4a51f533ddd6089c4336a9221e3aef2e93d2655a
Toni <garcia.t20@gmail.com>
2022-05-01 10:45:04 +0200

    first seeder pending of model to do inserts

---
+++ database/seeders/MoviesSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class MoviesSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Schema::disableForeignKeyConstraints();
+        DB::table('movies')->truncate();
+        Schema::enableForeignKeyConstraints();
+
+        $csvFile = fopen(base_path("database/seeders/movies.csv"), "r");
+        $firstline = true;
+
+        while ($data = fgetcsv($csvFile, 0, ",") !== FALSE) {
+            if (!$firstline) {
+                Movies::create([
+                    // Data at current CSV
+                    // Title,release_date,runtime,genre/s,description,image,trailer,actors,director,writers
+                ]);
+            }
+            $firstline = false;
+        }
+
+        fclose($csvFile);
+    }
+}
