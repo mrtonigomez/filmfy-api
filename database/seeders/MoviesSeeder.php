326b1c08d855c06ae5084f7df4eb48bdf85390eb
ToniGar20 <tgarcia@habitissimo.com>
2022-05-06 21:33:32 +0200

    added seed for categories-movies table

--- database/seeders/MoviesSeeder.php
+++ database/seeders/MoviesSeeder.php
@@ -23,18 +23,26 @@ class MoviesSeeder extends Seeder
         $firstline = true;
         while (($data = fgetcsv($csvFile, 0, ",")) !== FALSE) {
             if (!$firstline) {
-                dump($data);
-                Movies::create([
+                $new_movie = Movies::create([
                     // Data at current CSV
-                    // Title,release_date,runtime,genre/s,description,image,trailer,actors,director,writers
+                    // Title,release_date,runtime,genre/s,categories_id,description,image,trailer,actors,director,writers
                     "title" => $data['0'],
-                    "description" => $data['4'],
+                    "description" => $data['5'],
                     "release_date" => date($data['1']),
-                    "image" => $data['5'],
+                    "image" => $data['6'],
                     "runtime" => $data['2'],
                     "status" => 1,
-                    "trailer" => $data['6']
+                    "trailer" => $data['7']
                 ]);
+                $new_movie->save();
+                $categories_array = explode(',', $data['4']);
+
+                foreach ($categories_array as $category) {
+                    DB::table('categories_movies')->insert([
+                        "movies_id" => $new_movie->id,
+                        "categories_id" => $category,
+                    ]);
+                }
             }
             $firstline = false;
         }
