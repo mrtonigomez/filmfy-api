df609d1a6808a88c3996b0fe4e04c58ad7a3fd7e
ToniGar20 <tgarcia@habitissimo.com>
2022-05-06 22:20:56 +0200

    creating file to call all seeders

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(CategorySeeder::class);
+        $this->call(CountrySeeder::class);
+        $this->call(MoviesSeeder::class);
+        $this->call(UsersSeeder::class);
+    }
+}
