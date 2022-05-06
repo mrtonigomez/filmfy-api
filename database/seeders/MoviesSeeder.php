6a08f07adc5e5aced08ed6678ffd648df0e50ac1
ToniGar20 <tgarcia@habitissimo.com>
2022-05-06 14:33:31 +0200

    seeder working for top movies

--- database/seeders/MoviesSeeder.php
+++ database/seeders/MoviesSeeder.php
@@ -18,7 +18,7 @@ class MoviesSeeder extends Seeder
     {
         Schema::disableForeignKeyConstraints();
 
-        $csvFile = fopen(base_path("database/seeders/movies.csv"), "r");
+        $csvFile = fopen(base_path("database/seeders/top-movies-data.csv"), "r");
 
         $firstline = true;
         while (($data = fgetcsv($csvFile, 0, ",")) !== FALSE) {
@@ -29,7 +29,7 @@ class MoviesSeeder extends Seeder
                     // Title,release_date,runtime,genre/s,description,image,trailer,actors,director,writers
                     "title" => $data['0'],
                     "description" => $data['4'],
-                    "release_date" => $data['1'],
+                    "release_date" => date($data['1']),
                     "image" => $data['5'],
                     "runtime" => $data['2'],
                     "status" => 1,
