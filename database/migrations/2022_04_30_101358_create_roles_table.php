eaf63958a258ab139cb12181927071f1d9cce8a9
ToniGar20 <tgarcia@habitissimo.com>
2022-05-07 10:39:22 +0200

    changing column name for roles

--- database/migrations/2022_04_30_101358_create_roles_table.php
+++ database/migrations/2022_04_30_101358_create_roles_table.php
@@ -15,7 +15,7 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
-            $table->string("type");
+            $table->string('name');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
         });
