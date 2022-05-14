5467dcf28f4055e85aca8160e1c2616d949cff3f
ToniGar20 <tgarcia@habitissimo.com>
2022-05-14 11:26:14 +0200

    new likes table

---
+++ database/migrations/2022_05_14_085218_create_likes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('lists_id');
+            $table->unsignedInteger('users_id');
+
+            $table->foreign('lists_id')
+                ->references('id')
+                ->on('lists')
+                ->onDelete('cascade');
+            $table->foreign('users_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+}
