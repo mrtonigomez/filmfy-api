023746e6670cd922435ce9a46d77d6e1d66da328
ToniGar20 <tgarcia@habitissimo.com>
2022-05-23 17:27:02 +0200

    fix at controller

--- app/Http/Controllers/api/CommentsRestController.php
+++ app/Http/Controllers/api/CommentsRestController.php
@@ -88,7 +88,7 @@ class CommentsRestController extends Controller
     public function recentComments()
     {
         $comments = DB::table("comments as c")
-            ->select('m.title as m_title','m.release_date as m_release', 'm.image as m_image', 'c.*')
+            ->select('m.title as m_title','m.release_date as m_release', 'm.image as m_image', 'c.*', 'u.name as u_name')
             ->leftJoin('movies as m','m.id' , '=', 'c.movies_id')
             ->leftJoin('users as u','u.id' , '=', 'c.users_id')
             ->orderBy("c.updated_at", "DESC")
