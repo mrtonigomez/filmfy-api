664b2d79cd3fb362b765fbef52704411d389d093
ToniGomez <agomezj@cifpfbmoll.eu>
2022-05-26 23:54:19 +0200

    Change cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,11 +17,6 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-            //Url a la que se le dará acceso en las peticiones
-            ->header("Access-Control-Allow-Origin", "*")
-            //Métodos que a los que se da acceso
-            ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-            //Headers de la petición
-            ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization");
+            ->header('Access-Control-Allow-Origin', '*');
     }
 }
