c520c63a0c84560bbf2b0417b986b4296cac0e03
ToniGomez <agomezj@cifpfbmoll.eu>
2022-05-28 18:38:57 +0200

    JWT Auth

--- config/app.php
+++ config/app.php
@@ -231,6 +231,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        //Customs
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
 
     ],
 
